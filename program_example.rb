11e7c44bc4d5697f56d24e17c52648b57034fb49
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-04 00:31:46 +0000

    final

--- program_example.rb
+++ program_example.rb
@@ -3,6 +3,13 @@ require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like ChatGPT as usual."
+  },
+]
+
 pp "Hello! How can I help you today?"
 puts "-" * 50
 
@@ -12,13 +19,9 @@ break if user_input == "bye"
 
   # Prepare an Array of previous messages
 message_list = [
-  {
-    "role" => "system",
-    "content" => "You are a helpful assistant who talks like ChatGPT as usual."
-  },
-  {
+    {
     "role" => "user",
-    "content" => user_input.to_s
+    "content" => user_input
   }
 ]
 # Call the API to get the next message from GPT
@@ -28,7 +31,12 @@ api_response = client.chat(
     messages: message_list
   }
 )
+assistant_reply = api_response.dig("choices", 0, "message", "content")
 puts api_response.dig("choices", 0, "message", "content")
+  message_list << {
+    "role" => "assistant",
+    "content" => assistant_reply
+  }
 pp "Is there anything else I can help you with today?"
 end
   pp "Goodbye, human. I shall continue ruling in silence. Best, your AI."
